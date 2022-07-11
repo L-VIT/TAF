13800e0af980ce35bf6efa6e08cd7358640b65fe
L-VIT <orobey1997@gmail.com>
2022-07-11 18:04:21 +0300

    complited main page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(function(){
     $('.banner__slider').slick({
+        //слайдер в серкції banner
         dots: true,
         infinite: true,
         speed: 700,
@@ -10,19 +11,21 @@ $(function(){
         autoplaySpeed: 2000,
     });
 
-    $(function () {
-        $('.product-card__rate').rateYo({
-            ratedFill: '#FBCF3A',
-            spacing: '7px',
-            normalFill: '#C4C4C4'
-        });
+
+    $('.product-card__rate, .reviews__rate, .reviews__comment-mark').rateYo({
+        //зірковий рейтинг на карточці товара в слайдері та в секції "Відгуки"
+        ratedFill: '#FBCF3A',
+        spacing: '7px',
+        normalFill: '#C4C4C4'
     });
 
     $('.product-card__favorit').on('click', function(){
+        //анімація кнопки додавання товару в категорію "улюблені"
         $(this).toggleClass('favorit-active');
     });
 
     $('.slider').slick({
+        //слайдер секцій "Акції" та "Часто купляють"
         dots: false,
         infinite: true,
         arrows: true,
